ef9e10fb683491ee75ceec4c414ed6a4484dccde
kaisa <kaisa.piipari@gmail.com>
2017-10-23 00:15:57 +0100

    Done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,21 +1,19 @@
 class LineItemsController < ApplicationController
     
     def create
-        @user = current_user
-        @item = params[:item_id]
 
-        if @user.current_cart == nil
-            @cart = @user.carts.create
-            @cart.add_item(@item).save
-            @user.current_cart = @cart
-            @user.save
+        if current_user.current_cart == nil
+            @cart = Cart.create(user_id: current_user.id)
+            @cart.add_item(params[:item_id]).save
+            current_user.current_cart = @cart
+            current_user.save
         else
-            @cart = Cart.find_by(user_id: @user.id)
-            @cart.add_item(@item)
-            @cart.save
+            @cart = current_user.current_cart
+            @line_item = @cart.add_item(params[:item_id])
+            @line_item.save
         end
 
-        redirect_to cart_path(@user.current_cart)
+        redirect_to cart_path(current_user.current_cart)
         
     end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,13 +10,13 @@ class Cart < ActiveRecord::Base
     end
 
     def add_item(item)
-        if self.line_items.exists?(item_id: item)
-            @line_item = LineItem.find_by(item_id: item)
+
+        if @line_item = self.line_items.find_by(item_id: item)
             @line_item.quantity += 1
-            self.line_items << @line_item
+            @line_item
         else
-            self.line_items.build(cart_id: self.id, item_id: item)
-        end       
+            @line_item = self.line_items.build(item_id: item)
+        end      
     end
 
 end
